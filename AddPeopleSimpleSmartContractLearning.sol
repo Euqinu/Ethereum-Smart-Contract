6528a88089d72c857bf9fcfc1b6988cf9729bbee
Unique Chhetri <uniquechhetrii@gmail.com>
2022-12-29 11:55:31 +0530

    Initial commit

---
+++ AddPeopleSimpleSmartContractLearning.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.5.0;
+
+
+contract AddPeople{
+
+    address owner;
+    constructor() public {
+        owner=msg.sender;
+    }
+    struct Person{
+        string _firstname;
+        string _lastname;
+    }
+
+    modifier isBoss{
+        require(msg.sender==owner);
+        _;
+    }
+
+    modifier isEven{
+        require(2+2==3);
+        _;
+    }
+
+    mapping(uint=>Person) people;
+
+    function addPerson(uint num,string memory firstname,string memory lastname) public isBoss isEven{
+        people[num]=Person(firstname,lastname);
+    }
+}
