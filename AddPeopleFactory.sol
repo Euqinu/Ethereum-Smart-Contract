3883d3b710c9758ff416b668cb294bed06a04e8c
Unique Chhetri <uniquechhetrii@gmail.com>
2023-01-06 12:26:20 +0530

    accessing another smart contract

--- AddPeopleFactory.sol
+++ AddPeopleFactory.sol
@@ -13,4 +13,14 @@ contract AddPeopleFactory {
         AddPeople contract1 = new AddPeople();
         addContractList.push(contract1);
     }
+    
+    function addNumberInAnotherContract(uint256 contractIndex, uint256 numToBeStored) public {
+        AddPeople addPeopleContract = addContractList[contractIndex];
+        addPeopleContract.storeNumber(numToBeStored);
+    }
+
+    function getNumber(uint256 contractIndex) public view returns(uint256){
+        AddPeople addPeopleContract = addContractList[contractIndex];
+        return addPeopleContract.retrieveNumber();
+    }
 }
