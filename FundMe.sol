d8e113137024ce44ec2ec269a28abe48e7c4c520
Unique Chhetri <uniquechhetrii@gmail.com>
2023-01-20 12:07:12 +0530

    added eth conversion and list of addresses funding the contract

--- FundMe.sol
+++ FundMe.sol
@@ -2,54 +2,40 @@
 
 pragma solidity ^0.8.0;
 
-interface AggregatorV3Interface {
-  function decimals() external view returns (uint8);
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-  function description() external view returns (string memory);
-
-  function version() external view returns (uint256);
-
-  function getRoundData(uint80 _roundId)
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
-
-  function latestRoundData()
-    external
-    view
-    returns (
-      uint80 roundId,
-      int256 answer,
-      uint256 startedAt,
-      uint256 updatedAt,
-      uint80 answeredInRound
-    );
-}
 
 contract FundMe {
 
-    int public number;
-    int minNum=50;
+    uint256 minNum=50 * 1e18;
+    address[] public funderList;
+    mapping(address=>uint256) public funderNameAndAmount;
+
 
     function fund() public payable {
-        number=5;
-        // require(msg.value > minNum,"less eth");
+        require(getConversionRate(msg.value) > minNum,"less eth");
+        funderList.push(msg.sender);
+        funderNameAndAmount[msg.sender]=msg.value;
     }
 
     function getPrice() public {
         // ABI
         // Address 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
+        
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
+        (,int price,,,)=priceFeed.latestRoundData();
+        return uint256(price );
     }
 
     function getVersion() public view returns(uint256){
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e);
         return priceFeed.version();
+    }
+    
+    function getConversionRate(uint256 ethAmount) public view returns(uint256){
+      uint256 ethPrice = getPrice();
+      uint256 rate = (ethPrice * ethAmount) /1e18;
+      return rate; 
     }
 
 
